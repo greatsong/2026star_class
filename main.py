3c5d5e9ee13b12dc4e63e6b695795ef948c59b9c
송석리(Song Sukree) <greatsong21@gmail.com>
2026-06-11 15:49:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ fig = px.scatter(df, x="온도", y="절대등급", color="유형명",
                  labels={"온도": "표면 온도(K)", "절대등급": "절대등급(작을수록 밝음)"})
 st.plotly_chart(fig)
 
-﻿import plotly.express as px
-import streamlit as st
-
 유형명 = {0:"갈색왜성", 1:"적색왜성", 2:"백색왜성",
          3:"주계열성", 4:"초거성", 5:"극대거성"}
 df["유형명"] = df["유형"].map(유형명)
